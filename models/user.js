bb8e5c42c15f15290170d62a91e9a59f3ed91e7e
Liam Stark <williamsigbinoba2@gmail.com>
2024-07-24 00:50:43 +0100

    wer

--- models/user.js
+++ models/user.js
@@ -11,16 +11,15 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         validate: [isEmail, 'Please enter a valid email']
-    },
-    username: {
-        type: String,
-        required: [true, 'Please enter your username'],
-        minlength: [6, 'Minimum length of password should be 6']
-    },
-    password: {
+    }, password: {
         type: String,
         required: [true, 'Please enter a password']
     },
+    username: {
+        type: String,
+        required: [true, 'Please enter your username']
+    },
+
 })
 
 const User = mongoose.model('user', userSchema);
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,42 +3,75 @@ const express = require("express");
 const routes = express.Router();
 const jwt = require("jsonwebtoken");
 const bCrypt = require('bcrypt');
-const { addUser, getUsers, getUser, handleErrors } = require('../../User_DataStore');
+// const { addUser, getUsers, getUser, handleErrors } = require('../../User_DataStore');
 
 // Login route
 routes.post("/signup", async (req, res) => {
-    const { email, username, password, token } = req.body;
-    try {
-      // const existingUser = await User.findOne({ email });
-      let id;
-  
-      // if (existingUser) {
-      //   id = existingUser._id.toString()
-      //   return res.status(400).json("Email is already registered");
-      // }
-      const tokenValAge = 50;
-      const Token = jwt.sign({ _id: id }, "monsterhalfbadbeatkillshotashtray", {
-        expiresIn: tokenValAge
-      })
-  
-      // console.log("This is the generated token:" + Token)
-      // await User.updateOne({ _id: id }, { $set: { token: Token } })
-  
-      const salt = await bCrypt.genSalt();
-      const hashedPassword = await bCrypt.hash(password, salt);
-      await addUser({ email, username, password: hashedPassword, token }); 
-      // const user = await User.create({ email, username, password: hashedPassword, token: Token })
-      // await user.save();
-      res.status(201).json({success: true, message: 'User registered successfully.'});
+  const { email, username, password, token } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    let id;
 
-    } catch (err) {
-      console.log(err.message);
-      if (err.message == "Cannot read properties of undefined (reading 'length')") {
-        res.status(400).send('Include your username');
-      }
-      else
-        res.status(400).send('Error, user not created: ' + err.message);
+    if (existingUser) {
+      id = existingUser._id.toString()
+      return res.status(400).json("Email is already registered");
     }
-  });
+    const tokenValAge = 50;
+    const Token = jwt.sign({ _id: id }, "monsterhalfbadbeatkillshotashtray", {
+      expiresIn: tokenValAge
+    })
 
-  module.exports = routes;
+    // console.log("This is the generated token:" + Token)
+    await User.updateOne({ _id: id }, { $set: { token: Token } })
+
+    const salt = await bCrypt.genSalt();
+    const hashedPassword = await bCrypt.hash(password, salt);
+    // await addUser({ email, username, password: hashedPassword, token }); 
+    const user = await User.create({ email, username, password: hashedPassword, token: Token })
+    await user.save();
+    res.status(201).json({ success: true, message: 'User registered successfully.' });
+
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      const errors = Object.keys(err.errors).reduce((acc, key) => {
+        acc[key] = err.errors[key].message;
+        return acc;
+      }, {});
+      res.status(400).json(errors);
+    } else {
+      res.status(500).send('Internal Server Error');
+    }
+  }
+});
+
+routes.post('/login', async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    if (!email || !username && !password) {
+      return res.status(400).json("Input your username or email and password");
+    }
+
+    const userExists = await User.findOne({ email, username });
+
+    if (!userExists) {
+      console.log(userExists)
+      return res.status(400).json("User doesn't exist");
+    }
+
+    const passwordValid = await bCrypt.compare(password, userExists.password);
+    if (!passwordValid) {
+      console.log("password not correct")
+      return res.status(400).json("Your password is incorrect");
+    }
+    let logTimer = setTimeout(navigate("/home"));
+    alert(`You'll be logged in - ${logTimer}`)
+    console.log("Logging in")
+    return res.status(200).json("Logging in");
+  } catch (err) {
+    res.status(500).send(err);
+    console.log(err);
+  }
+});
+
+module.exports = routes;
