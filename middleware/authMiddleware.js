4bc2d41a185ddf6302fc783b1dce82bd33a8a2c3
LeeAmStark <williamsigbinoba2@gmail.com>
2024-07-25 08:23:13 +0100

    JWT Authentication Complete - For Backend Auth

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1]; // Splits between "Bearer" & the jwt 
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+      } else {
+        req.user = decodedToken;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+};
+
+module.exports = { requireAuth };
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,35 +3,30 @@ const express = require("express");
 const routes = express.Router();
 const jwt = require("jsonwebtoken");
 const bCrypt = require('bcrypt');
-// const { addUser, getUsers, getUser, handleErrors } = require('../../User_DataStore');
+const { requireAuth } = require('../../middleware/authMiddleware.js');
 
-// Login route
+// Signup route
 routes.post("/signup", async (req, res) => {
-  const { email, username, password, token } = req.body;
+  const { email, username, password } = req.body;
   try {
-    
     const existingUser = await User.findOne({ email });
-    let id;
-
+    
     if (existingUser) {
-      id = existingUser._id.toString()
       return res.status(400).json("Email is already registered");
     }
-    const tokenValAge = 50;
-    const Token = jwt.sign({ _id: id }, "monsterhalfbadbeatkillshotashtray", {
-      expiresIn: tokenValAge
-    })
-
-    // console.log("This is the generated token:" + Token)
-    await User.updateOne({ _id: id }, { $set: { token: Token } })
 
     const salt = await bCrypt.genSalt();
     const hashedPassword = await bCrypt.hash(password, salt);
-    // await addUser({ email, username, password: hashedPassword, token }); 
-    const user = await User.create({ email, username, password: hashedPassword, token: Token })
-    await user.save();
-    res.status(201).json({ success: true, message: 'User registered successfully.' });
 
+    const user = new User({ email, username, password: hashedPassword });
+    await user.save();
+
+    // Generate JWT
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.SECRET_KEY, { expiresIn: '1h' });
+    user.token = token;
+    await user.save();
+
+    res.status(201).json({ success: true, message: 'User registered successfully.', token });
   } catch (err) {
     if (err.name === 'ValidationError') {
       const errors = Object.keys(err.errors).reduce((acc, key) => {
@@ -45,21 +40,19 @@ routes.post("/signup", async (req, res) => {
   }
 });
 
+// Login route
 routes.post('/login', async (req, res) => {
   try {
-    const { email, username, password } = req.body;
+    const { email, password } = req.body;
 
     if (!email || !password) {
       return res.status(400).json("Input your email and password");
     }
 
-    const user = await User.findOne({ username });
-    const e_mail = await User.findOne({email});
+    const user = await User.findOne({ email });
 
     if (!user) {
       return res.status(400).json("User doesn't exist");
-    }else if(!e_mail){
-      return res.status(400).json("Email doesn't exist");
     }
 
     const passwordValid = await bCrypt.compare(password, user.password);
@@ -67,8 +60,8 @@ routes.post('/login', async (req, res) => {
       return res.status(400).json("Your password is incorrect");
     }
 
-    // Generate token
-    const token = jwt.sign({ _id: user._id }, "your_secret_key", { expiresIn: '1h' });
+    // Generate JWT
+    const token = jwt.sign({ id: user._id, username: user.username }, process.env.SECRET_KEY, { expiresIn: '1h' });
 
     res.status(200).json({ success: true, message: "Logged in successfully", token });
   } catch (err) {
